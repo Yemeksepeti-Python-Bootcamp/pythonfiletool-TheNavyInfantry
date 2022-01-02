8cd3b22c0175c8a9523a8a7c964535ee05fc3d73
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-02 22:27:07 +0100

    ADD FileTool.py

--- FileTool.py
+++ FileTool.py
@@ -1,1 +1,252 @@
+from pathlib import Path
+import csv, json
 
+class FileToolClass(object):
+
+    def __init__(self,file_path,*fields,):
+        self.file_path = file_path
+        self.fields = fields
+
+    def is_exists(self):
+        if not Path(self.file_path).is_file():
+            with open(self.file_path.split("/")[-1], 'a', newline='') as csv_file: #possible to use w+ instead of a
+                    writer = csv.writer(csv_file)
+                    writer.writerows(self.fields)
+            print("File CREATED and header inputs were written inside the file!")
+
+        else:
+            print("File EXISTS and ready to use!")
+
+    def get_header(self):
+        with open(self.file_path, 'r') as csv_file:
+            header = []
+            reader = csv.reader(csv_file)
+            for row in reader:
+                header.append(row)
+
+            if header[0] == []:
+                return len(header[1])
+            else:
+                return len(header[0])
+
+
+    def read_in_file(self):
+        with open(self.file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            # for row in reader:
+            #     print(row)
+            for row, display in enumerate(reader, start=0):
+                print(f"{row} |", *display)
+
+    def search_in_file(self,to_find):
+        with open(self.file_path, 'r') as csv_file:
+            match_list = []
+            reader = csv.reader(csv_file)
+            for row, search in enumerate(reader):
+                for each in search:
+                    if to_find in each.lstrip():
+                        match_list.append(f"Your query was found on row {row} : (as) {each.lstrip()}")
+            print(*match_list, sep='\n')
+
+    def delete_in_file(self, row_to_delete):
+        safe_lines = []
+        with open(self.file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+
+            for row_number, row in enumerate(reader, start=0):
+                if row_number != row_to_delete:
+                    safe_lines.append(row)
+
+        with open(self.file_path, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(safe_lines)
+
+        print(f"Row {row_to_delete} is deleted from your file")
+
+    def append_on_file(self,*value_to_append):
+        to_append = []
+        to_append.append(value_to_append)
+
+        with open(self.file_path, 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            if len(value_to_append) == self.get_header():
+                writer.writerow(*[i for i in to_append])
+                print("Append process was successful!")
+            else:
+                print(f"Too much inputs were given! Try again!")
+
+        self.read_in_file()
+
+    def update_in_file(self,row_to_update: int,*value_to_change):
+        updated_csv = []
+        with open(self.file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+
+
+            if len(value_to_change) == self.get_header():
+                for row, search in enumerate(reader):
+                    if row_to_update == row:
+                        search = list(value_to_change[:]) #gets the whole list with :
+                    updated_csv.append(search)
+                print(*updated_csv, sep="\n")
+
+            else:
+                for row, search in enumerate(reader):
+                    if row_to_update == row:
+                        search = search
+                    updated_csv.append(search)
+                print(f"Too much inputs were given! Try again!")
+
+
+        with open(self.file_path, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(updated_csv)
+
+
+    def convert_JSON(self,option):
+        json_arr = []
+
+        with open(self.file_path, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            for row in reader:
+                json_arr.append(row)
+
+        generate_json_path = (self.file_path.split("/")[-1]).replace("csv","json")
+
+        with open(generate_json_path, 'w') as json_file:
+            json_string = json.dumps(json_arr, indent=3)
+            json_file.write(json_string)
+
+        if option == "all":
+            print(json_arr)
+        else:
+            try:
+                single_row = json_arr[int(option)]
+                print(single_row)
+            except IndexError as e:
+                print(f"Invalid index request! : {e}")
+
+    def merge_files(self,from_file,target_file):
+        from_file_path = Path(from_file)
+        target_file_path = Path(target_file)
+
+        if from_file_path.suffix and target_file_path.suffix == ".csv":
+
+                headers = []
+                data = []
+
+                try:
+                    for file_name in [from_file,target_file]:
+                        with open(f'{file_name}','r') as f:
+                            temp_csv = csv.DictReader(f)
+                            headers += temp_csv.fieldnames
+                            for row in temp_csv:
+                                data.append(row)
+
+                    headers = sorted(set(headers), key=headers.index) #'key=headers.index' maintains header's real order
+                    with open(target_file, 'w') as f:
+                        writer = csv.DictWriter(f, fieldnames=headers)
+                        writer.writeheader()
+                        writer.writerows(data)
+
+                    print("Successful!")
+
+                except FileNotFoundError as e:
+                    print("Failed!",e)
+
+
+        elif from_file_path.suffix and target_file_path.suffix == ".json":
+
+            result = list()
+
+            try:
+                with open(from_file, 'r') as in_from_file, open(target_file, 'r') as in_target_file:
+                    result.extend(json.load(in_from_file))
+                    result.extend(json.load(in_target_file))
+
+                with open(target_file, 'w') as in_target_file:
+                    json.dump(result, in_target_file, indent=3)
+
+                print("Successful!")
+
+            except FileNotFoundError as e:
+                print("Failed!", e)
+
+
+def menu():
+    ask_path = input("Path: ")
+    ask_fields_list = []
+
+    while True:
+        ask_a_field = input("Field(s) [quit]: ")
+        if "quit" == ask_a_field:
+            break
+        ask_fields_list.append(ask_a_field)
+
+    obj = FileToolClass(str(ask_path),ask_fields_list)
+
+    # obj = FileToolClass("airtravel.csv", "Month", "1958", "1959", "1960")
+    while True:
+        print(
+            """
+            1) Create/Open file 
+            2) Read file
+            3) Search in file
+            4) Add to file 
+            5) Delete in file
+            6) Update in file
+            7) Convert to JSON
+            8) File merge
+            0) Quit
+            """
+        )
+        choice = int(input("Choice: "))
+
+        if choice == 1:
+            obj.is_exists()
+
+        elif choice == 2:
+            obj.read_in_file()
+
+        elif choice == 3:
+            user_input = str(input("Search: "))
+            obj.search_in_file(user_input)
+
+        elif choice == 4:
+            pass
+
+
+        elif choice == 5:
+            delete_row = int(input("Delete: "))
+            obj.delete_in_file(delete_row)
+
+        elif choice == 6:
+            updated = []
+            update_row = int(input("Row # to update: "))
+            while True:
+                if len(updated) == obj.get_header():
+                    break
+                update_data = input("Data to update: ")
+                updated.append(update_data)
+
+            obj.update_in_file(update_row,*updated)
+
+
+        elif choice == 7:
+            convert_option = input("Convert all file (all) OR "
+                                   "Convert a specific row (#) : ")
+
+            obj.convert_JSON(convert_option)
+
+
+        elif choice == 8:
+            from_file_name = str(input("Enter the filename you want to get data from (EX: xyz.csv / xyz.json): "))
+            target_file_name = str(input("Enter the target file name you want to merge (EX: xyz.csv / xyz.json): "))
+
+            obj.merge_files(from_file_name,target_file_name)
+
+        elif choice == 0:
+            break
+
+menu()
